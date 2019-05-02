6a953f1457fb159a5aeb617b837012e44a25dd28
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-05-02 13:33:03 +0300

    Added viewmodels and a test textbox and textblock.

---
+++ HTextEditor/HTextEditorWPF/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PropertyChanged;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace HTextEditorWPF.ViewModels.Base
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to run a <see cref="PropertyChanged"/> event.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
